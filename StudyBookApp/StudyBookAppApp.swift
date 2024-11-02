1b30f94cfb00cb782375e8033cc1c5b26fb82eb4
amefure <shachi10969@gmail.com>
2024-11-02 23:22:26 +0900

    Initial Commit

---
+++ StudyBookApp/StudyBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBookAppApp.swift
+//  StudyBookApp
+//
+//  Created by t&a on 2024/11/02.
+//
+
+import SwiftUI
+
+@main
+struct StudyBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
